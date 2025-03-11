9261ce40c81da74899cbfa5094b194f219e0a0e6
Shvam Singh <singhshivam583@yahoo.in>
2025-03-12 00:31:21 +0530

    fix:ui fixes in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ function Footer() {
     <div className=" flex items-center justify-center  bg-white dark:bg-black">
       <footer className="text-center w-full max-w-3xl dark:text-gray-200 font-mono text-gray-500 text-sm border-t border-gray-300 dark:border-gray-700 pb-2">
         {/* Social Icons */}
-        <SocialIcons className="justify-center mb-4 mt-4" />©{" "}
+        <SocialIcons className="justify-center md:mb-4 mb-20 mt-4"/>©{" "}
         {new Date().getFullYear()} Web3ngineer. All rights reserved.
       </footer>
     </div>
